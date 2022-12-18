83e0d9c3e6bb319cb89a5f612e90ee0ba564a940
Shun Git <kwokshungit@gmail.com>
2022-12-18 15:41:47 +0800

    refactor HS core
    closes #3

--- lib/hs/types.d.ts
+++ lib/hs/types.d.ts
@@ -1,3 +1,24 @@
+/**
+ * @param {string} data string to hash
+ * @param {string} algo hash algorithm
+ */
+export type Hasher = (data: string, algo?: string) => Promise<string>
+
+export interface SignerOptions {
+	algorithm: string
+	hashAlgorithm: string
+
+	/** additional headers to sign */
+	headers: string[]
+
+	/**
+	 * validity of signature (seconds)
+	 *
+	 * used by HS2019 to calculate the `expires` field
+	 */
+	validity?: number
+}
+
 export interface SignatureField {
 	keyId: string
 	signature: string
@@ -8,4 +29,11 @@ export interface SignatureField {
 	created?: number
 	/** expiry timestamp */
 	expires?: number
+}
+
+export interface KeyFetchOptions {
+	/** signer instance */
+	signer: Signer
+	/** additional headers */
+	headers: Record<string, string>
 }
